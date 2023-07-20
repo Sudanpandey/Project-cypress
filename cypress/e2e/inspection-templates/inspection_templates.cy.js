008e18f0e6eebf822be1e6e6da3b3949601ed065
Madhusudan Pandey <madhusudan.pandey@proshore.eu>
2023-07-20 15:17:47 +0545

    Add inspection template feature

---
+++ cypress/e2e/inspection-templates/inspection_templates.cy.js
@@ -0,0 +1,102 @@
+import 'cypress-file-upload';
+const editInspectionTitle = 'Inspection Template' + Date.now();
+
+describe("Inspection Templates", () => {
+    it("Should display error message for empty template name", () => {
+        cy.login();
+        // Navigation
+        cy.get(':nth-child(11) > .nav-item > .icon-sidenav',{timeout: 20000}).click();
+        cy.get('.nav > :nth-child(4) > .nav-link').click();
+        /*check empty template name  name field validation */
+        cy.get('div[class="d-none d-sm-block"]').click();
+        cy.get('.form-group > .form-control').clear();
+        cy.get('.card-footer > .btn-secondary').click();
+        // cy.contains('Please fill in this field.').click();
+      })
+    it("Should be listed at the top of the page after creating new contracts", () => {
+        cy.login();
+        // Navigation
+        cy.get(':nth-child(11) > .nav-item > .icon-sidenav',{timeout: 20000}).click();
+        cy.get('.nav > :nth-child(4) > .nav-link').click();
+        //Create new inspection template
+        cy.get('div[class="d-none d-sm-block"]').click();
+        cy.get('.form-group > label').should('contain','Sjabloonnaam');
+        cy.get('.card-footer > .btn-secondary').click();
+        cy.get('.notification-content').should('contain','The template was successfully added'); 
+        cy.get('tbody>tr:nth-child(1)').should('contain','Inspection title');
+        // cy.get('tbody').find('tr').its('length').should('be.eq', 1);
+
+      })
+    it("Should edit the template name and add a leaf node to the default category", () => {
+        cy.login();
+        // Navigation
+        cy.get(':nth-child(11) > .nav-item > .icon-sidenav',{timeout: 20000}).click();
+        cy.get('.nav > :nth-child(4) > .nav-link').click();
+        //Create new inspection template
+        cy.get('div[class="d-none d-sm-block"]').click();
+        cy.get('.form-group > label').should('contain','Sjabloonnaam');
+        cy.get('.form-group > .form-control').clear();
+        cy.get('.form-group > .form-control').type(editInspectionTitle);
+        cy.contains('Sample category').click();
+        cy.get('[title="Add Leaf Node"] > .icon > .mdi').click();
+        cy.get('.vtl-node-content').should('contain',' New item ');
+    })
+    it("Should add a new category and new entry under that category", () => {
+        cy.login();
+        // Navigation
+        cy.get(':nth-child(11) > .nav-item > .icon-sidenav',{timeout: 20000}).click();
+        cy.get('.nav > :nth-child(4) > .nav-link').click();
+        //Create new inspection template
+        cy.get('div[class="d-none d-sm-block"]').click();
+        cy.get('.form-group > label').should('contain','Sjabloonnaam');
+        cy.contains('categorie toevoegen').click();
+        cy.get('.vtl-node-content').contains('New category');
+        cy.get('.vtl-operation').invoke('removeAttr', 'style');
+        cy.get('.vtl-operation span[title="Add Leaf Node"]').eq(1).click();
+        cy.contains('.vtl-node-content', 'New item');
+        // cy.contains('button', 'categorie toevoegen').click();
+        // cy.get('.vtl-operation span[title="Add Leaf Node"]:first').click();
+        // cy.get('span[title="Add Leaf Node"]').click();
+        // cy.contains('New category').click();
+        // cy.get('form-group form-group-focus > .btn-secondary').click();
+        // cy.get('[title="edit"] > .icon > .mdi mdi-pencil').click();
+    })
+    it("Should edit the category and associated entry", () => {
+        cy.login();
+        // Navigation
+        cy.get(':nth-child(11) > .nav-item > .icon-sidenav',{timeout: 20000}).click();
+        cy.get('.nav > :nth-child(4) > .nav-link').click();
+        //Edit default inspection template
+        cy.get('div[class="d-none d-sm-block"]').click();
+        cy.get('.form-group > label').should('contain','Sjabloonnaam');
+        cy.contains('.vtl-node-content', 'Sample category');
+        // cy.get('.vtl-operation').invoke('removeAttr', 'style');
+        cy.get('.vtl-operation').first().invoke('removeAttr', 'style');
+        cy.get('span[title="edit"]').eq(0).click();
+        cy.get('.vtl-input').clear().type("Wall inspection");
+        // cy.get('.form-group > label').should('contain','Sjabloonnaam').click();vtl
+        cy.get('.mdi.mdi-menu.mr-2').click();
+        //Edit catagory items
+        cy.contains('.vtl-node-content', ' Inspection item 1 ');
+        cy.get('.vtl-operation').eq(1).invoke('removeAttr', 'style');
+        cy.get('.vtl-operation span[title="edit"]').eq(1).click();
+        cy.get('.vtl-input').clear().type("Floor Item -one Ins");  
+        cy.get('.mdi.mdi-menu.mr-2').click();
+        cy.get('.vtl-operation').eq(2).invoke('removeAttr', 'style');
+        cy.get('.vtl-operation span[title="edit"]').eq(2).click();
+        cy.get('.vtl-input').clear().type("Floor Item -Two Ins");
+        cy.get('.mdi.mdi-menu.mr-2').click();
+    })
+    it.only("Should redirect to the inspection template page when clicking the back button", () => {
+        cy.login();
+        // Navigation
+        cy.get(':nth-child(11) > .nav-item > .icon-sidenav',{timeout: 20000}).click();
+        cy.get('.nav > :nth-child(4) > .nav-link').click();
+        /*check redirect to the inspection template page */
+        cy.get('div[class="d-none d-sm-block"]').click();
+        cy.get('button.btn-primary').contains('Terug').click();
+        cy.get('div.d-flex.align-items-center').contains('Note: De inspectiesjabloon die momenteel in gebruik is en niet kan worden verwijderd.');
+
+    })
+})
+
